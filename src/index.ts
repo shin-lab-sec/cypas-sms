3e620d3712aa78e48328f15784c691d3b9de5baf
t-kabu <t.kabu.03@gmail.com>
2022-08-22 22:01:55 +0900

    feat: ターミナルのスタートとストップのapiを作成

--- src/index.ts
+++ src/index.ts
@@ -45,3 +45,43 @@ app.post('/docker', async (req, res) => {
     res.status(500).json(result)
   }
 })
+
+app.post('/terminal/start', async (req, res) => {
+  const key = req.body.key
+
+  let nextPorts
+  const usedPorts = await client.get('usedPorts')
+  if (usedPorts) {
+    nextPorts = Number(usedPorts) + 1
+  } else {
+    nextPorts = 30000
+  }
+  await client.set('usedPorts', nextPorts)
+  await client.set(key, nextPorts)
+
+  let result
+  try {
+    result = await dockerCommand(
+      `run -p ${nextPorts}:3000 --name ${key}  wetty yarn start --ssh-host 127.0.0.1 --base / --allow-iframe true`,
+    )
+    res.status(200).json(result)
+  } catch (e) {
+    result = { message: (e as Error).message }
+    res.status(500).json(result)
+  }
+})
+
+app.post('/terminal/delete', async (req, res) => {
+  const key = req.body.key
+
+  await client.del(key)
+
+  let result
+  try {
+    result = await dockerCommand(`container rm ${key} -f`)
+    res.status(200).json(result)
+  } catch (e) {
+    result = { message: (e as Error).message }
+    res.status(500).json(result)
+  }
+})
